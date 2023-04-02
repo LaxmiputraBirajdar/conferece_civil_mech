21b95ee747b850a25a48e113333256ab31bc3e17
LaxmiputraBirajdar <laxmiputrabirajdar1515@gmail.com>
2023-04-02 18:05:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const ejs = require("ejs")
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res)
+{
+
+  res.render("home");
+})
+
+app.get('/:page', function(req, res) {
+  var page = req.params.page;
+  res.render(page);
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Runninig on the port 3000");
+})
